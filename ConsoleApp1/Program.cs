dd65666326f9976e58e536e0e7769bc3813dff62
dubrownik <karola2234@gmail.com>
2022-12-01 23:22:43 +0100

    Główna pętla programu

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,35 @@
     {
         public static void Main()
         {
-            
+            while (true)
+            {
+                Console.WriteLine("Saldo: ");
+
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Wprowadź transakcję");
+                Console.WriteLine("2. Zarządzaj kategoriami");
+                Console.WriteLine("3. Zarządzaj osobami");
+                Console.WriteLine("4. Podsumowanie");
+                Console.WriteLine("0. Koniec");
+                string wybór = Console.ReadLine();
+
+                switch (wybór)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Niepoprawny wybór! Spróbuj jeszcze raz.");
+                        break;
+                }
+            }
         }
     }
 }
